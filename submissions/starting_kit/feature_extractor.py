bdb7af733ea99a6f82a62130de282fb1ea92d326
Anas Barakat <anas.barakat@telecom-paristech.fr>
2018-01-26 18:41:03 +0100

    feature extractor

--- submissions/starting_kit/feature_extractor.py
+++ submissions/starting_kit/feature_extractor.py
@@ -1,66 +1,33 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
+import pandas as pd
+import numpy as np 
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-
-def document_preprocessor(doc):
-    """ A custom document preprocessor
-
-    This function can be edited to add some additional
-    transformation on the documents prior to tokenization.
-
-    At present, this function passes the document through
-    without modification.
-    """
-    return doc
-
-
-def token_processor(tokens):
-    """ A custom token processor
-
-    This function can be edited to add some additional
-    transformation on the extracted tokens (e.g. stemming)
-
-    At present, this function just passes the tokens through.
-    """
-    for t in tokens:
-        yield t
-
-
-class FeatureExtractor(TfidfVectorizer):
-    """Convert a collection of raw docs to a matrix of TF-IDF features. """
+class FeatureExtractor():
 
     def __init__(self):
-        # see ``TfidfVectorizer`` documentation for other feature
-        # extraction parameters.
-        super(FeatureExtractor, self).__init__(
-                analyzer='word', preprocessor=document_preprocessor)
-
+        return self
+    
+    def dummify(df):
+        dum_dep = pd.get_dummies(df['department'])
+        df = df.drop('department', axis=1)
+        df = df.join(dum_dep)
+        #add dummy variable
+        #df = data.join(datanew)
+        dum_sal = pd.get_dummies(df['salary'])
+        df = df.drop('salary', axis=1)
+        df = df.join(dum_sa)
+        #df = data.join(datanew)
+        return (df)
+    
     def fit(self, X_df, y=None):
-        """Learn a vocabulary dictionary of all tokens in the raw documents.
-
-        Parameters
-        ----------
-        X_df : pandas.DataFrame
-            a DataFrame, where the text data is stored in the ``statement``
-            column.
-        """
-        super(FeatureExtractor, self).fit(X_df.statement)
         return self
 
     def fit_transform(self, X_df, y=None):
-        return self.fit(X_df).transform(X_df)
+        return self.transform(X_df)
 
     def transform(self, X_df):
-        X = super(FeatureExtractor, self).transform(X_df.statement)
+        X = self.dummify(X_df)
         return X
 
-    def build_tokenizer(self):
-        """
-        Internal function, needed to plug-in the token processor, cf.
-        http://scikit-learn.org/stable/modules/feature_extraction.html#customizing-the-vectorizer-classes
-        """
-        tokenize = super(FeatureExtractor, self).build_tokenizer()
-        return lambda doc: list(token_processor(tokenize(doc)))
+
