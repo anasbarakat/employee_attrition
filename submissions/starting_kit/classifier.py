0af1a02afaff6fc3c573c0a59669ba2979844b42
Anas Barakat <anas.barakat@telecom-paristech.fr>
2018-01-26 18:45:29 +0100

    basic classifier

--- submissions/starting_kit/classifier.py
+++ submissions/starting_kit/classifier.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 from sklearn.base import BaseEstimator
-from sklearn.ensemble import RandomForestClassifier
-
+#from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
 class Classifier(BaseEstimator):
     def __init__(self):
-        self.clf = RandomForestClassifier()
+        #self.clf = RandomForestClassifier()
+        self.clf = LogisticRegression()
 
     def fit(self, X, y):
-        self.clf.fit(X.todense(), y)
+        #self.clf.fit(X.todense(), y)
+        self.clf.fit(X, y)
 
     def predict(self, X):
-        return self.clf.predict(X.todense())
+        return self.clf.predict(X)
 
     def predict_proba(self, X):
         return self.clf.predict_proba(X)
